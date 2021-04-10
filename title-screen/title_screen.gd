74448ea615a0ff934059ea81c8f1025b6d023280
James Sharkey <james.hg.sharkey@gmail.com>
2021-04-10 22:08:21 +0100

    Add main menu and stub out options menu

---
+++ title-screen/title_screen.gd
@@ -0,0 +1,16 @@
+extends Control
+
+var scene_path_to_load
+
+func _ready():
+	$Menu/CenterRow/Buttons/NewGameButton.grab_focus()
+	for button in $Menu/CenterRow/Buttons.get_children():
+		button.connect("pressed", self, "on_Button_pressed", [button.scene_to_load])
+
+func on_Button_pressed(scene_to_load):
+	scene_path_to_load = scene_to_load
+	$FadeIn.show()
+	$FadeIn.fade_in()
+
+func _on_FadeIn_fade_finished():
+	get_tree().change_scene(scene_path_to_load)
