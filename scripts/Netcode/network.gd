3fda15e51d5c841ac3aec9fd925608a2d4611fd6
James Wild <jwild2@sheffield.ac.uk>
2021-04-11 10:39:33 +0100

    Persistent Scores, scrolling background (#1)
    * Moved score to player. Added highscore that is persistent between games.
    
    * Bug squashing.
    
    * Added scrolling backfground
    
    * First pass at multiplayer - connection and character sync ok, walls need to be mirrored from host
    
    * Added your IP to the host waiting screen
    
    * Revert changes
    
    * Added watermelon
    
    * Added a few new potential wallpaper textures, of varying quality
    
    * Addressing Jordan's comments.
    
    * Minor changes to names
    
    * Disable the bloody offline testing
    
    * Close the save file
    
    Co-authored-by: Jibbajabbafic <Jibbajabbafic@users.noreply.github.com>
    
    * Addressing JibbaJabba's concerns
    
    * Update scripts/controllers/World.gd
    
    Co-authored-by: Jibbajabbafic <Jibbajabbafic@users.noreply.github.com>

---
+++ scripts/Netcode/network.gd
@@ -0,0 +1,49 @@
+extends Node
+
+const RPC_PORT = 31400
+const MAX_PLAYERS = 2
+
+var net_id = null
+var is_host = false
+var peer_ids = []
+var current_players = 1
+var host_player
+var is_online = true
+
+
+func initialise_server():
+	is_host = true
+	
+	# This starts the actual server to communicate with clients
+	var peer = NetworkedMultiplayerENet.new()
+	peer.create_server(RPC_PORT, MAX_PLAYERS)
+	get_tree().network_peer = peer
+	
+	is_online = true
+
+
+func initialise_client(server_ip):
+	is_host = false
+	
+	# Start a client for communicating with the server, and connect to that server
+	var peer = NetworkedMultiplayerENet.new()
+	peer.create_client(server_ip, RPC_PORT)
+	get_tree().network_peer = peer
+	
+	is_online = true
+
+func set_ids():
+	if is_online:
+		net_id = get_tree().get_network_unique_id()
+		peer_ids = get_tree().get_network_connected_peers()
+	else:
+		net_id = 0
+		peer_ids = []
+
+
+func get_my_ip():
+	var myIP = '127.0.0.1'
+	for ip in IP.get_local_addresses():
+		if (not ip.begins_with("127")) and ip.count(".") == 3:
+			myIP = ip
+	return myIP
---
+++ scripts/controllers/World.gd
@@ -0,0 +1,122 @@
+extends Node2D
+
+var Wall = preload("res://scenes/Wall.tscn")
+var height_range = 100
+var gap_range_min = 130
+var gap_range_max = 250
+
+onready var Background = $Background
+onready var HiScore = $UI/HighScore
+onready var Score = $UI/Score
+
+var high_score_fname = "user://highscore.save"
+
+onready var WallSpawnTimer = $WallSpawnTimer
+
+var wall_spawn_time = 2.5
+var start_wall_speed = 4.0
+var wall_speed = start_wall_speed
+var speed_up = 0.1
+var scroll_speed = 0.1
+var bob_speed = 1.0
+var bob_amplitude = 0.01
+
+
+func _ready():
+	# Setup gubbins
+	Globals.high_score = load_high_score()
+	HiScore.text = Globals.high_score()
+	
+	# Set the wallpaper motion
+	Background.material.set_shader_param('scroll_speed', scroll_speed)
+	Background.material.set_shader_param('bob_speed', bob_speed)
+	Background.material.set_shader_param('bob_amplitude', bob_amplitude)
+		
+	WallSpawnTimer.wait_time = wall_spawn_time
+	WallSpawnTimer.start()
+	
+	# Give all the players an ID
+	Net.set_ids()
+	# and create a player sprite for each.
+	create_players()
+	# Then, create my own lil guy
+	$Player.initialise(Net.net_id)
+
+func create_players():
+	for id in Net.peer_ids:
+		create_player(id)
+
+func create_player(id):
+	var p = preload("res://scenes/Player.tscn").instance()
+	add_child(p)
+	p.initialise(id)
+
+func reset_game():
+	# This works well enough for one player, but we actually want a less nuclear approach
+	var _ok = get_tree().reload_current_scene()
+
+
+#### Wall functions
+func spawn_wall():
+	var inst = Wall.instance()
+	inst.position = Vector2(get_viewport().size.x / 2 + 64, rand_range(-height_range, height_range))
+	inst.gap = rand_range(gap_range_min, gap_range_max)
+	inst.speed = wall_speed
+	call_deferred("add_child", inst)
+
+func _on_WallSpawner_timeout():
+	WallSpawnTimer.wait_time = wall_spawn_time * float(start_wall_speed) / wall_speed
+	spawn_wall()
+
+
+#### Player helper functions
+func _on_Player_death(player):
+	# See if we have a new PB
+	if player.score > Globals.high_score:
+		Globals.high_score = player.score
+		save_high_score()
+		HiScore.text = str(Globals.high_score)
+	
+	# Tell the engine it can lose the player
+	player.queue_free()
+	
+	reset_game()
+
+func _on_Player_score_point(player):
+	# Actual incrimenting is handled on the player object
+	increase_difficulty()
+	Score.text = str(player.score)
+
+
+#### World functions
+func increase_difficulty():
+	wall_speed += speed_up
+	# Speed up the background
+	# TODO: this causes jerky stutter. Needs fixing.
+	# Background.material.set_shader_param('scroll_speed', wall_speed*parallax)
+	for wall in get_tree().get_nodes_in_group("walls"):
+		wall.speed = wall_speed
+
+func load_high_score():
+	var save_file = File.new()
+	if not save_file.file_exists(high_score_fname):
+		return 0
+	
+	save_file.open(high_score_fname, File.READ)
+	var data = parse_json(save_file.get_as_text())
+	save_file.close()
+	
+	var high_score = int(data['highscore'])
+	
+	return high_score
+
+func save_high_score():
+	var save_file = File.new()
+	save_file.open(high_score_fname, File.WRITE)
+	
+	# We will store as a JSON object. Overkill for a single integer, but should 
+	# be easy to scale out.
+	var store_dict = {"highscore": Globals.high_score}
+	
+	save_file.store_line(to_json(store_dict))
+	save_file.close()
