bf42f3cde59454d3c8372e183f521954f2f04957
Jibbajabbafic <Jibbajabbafic@users.noreply.github.com>
2021-04-19 21:48:47 +0100

    refactor(dir): separate menu scenes

--- client/singletons/scene_manager.gd
+++ client/singletons/scene_manager.gd
@@ -2,9 +2,9 @@ extends Node
 
 onready var _scenes = {
 	Enums.Scene.WORLD: preload("res://client/world/world.tscn"),
-	Enums.Scene.TITLE: preload("res://client/menu/title_screen.tscn"),
+	Enums.Scene.TITLE: preload("res://client/menu/title/title_screen.tscn"),
 }
 
 func change_to(scene : int):
-	assert(scene in Enums.Scene.values(), "Argument must be a valid 'Scene' enum value!")
+	assert(scene in Enums.Scene.values(), "Argument must be a valid 'Enums.Scene' enum value!")
 	assert(get_tree().change_scene_to(_scenes[scene]) == OK, "Error when changing scene to " + str(scene))
