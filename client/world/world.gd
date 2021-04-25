d911442515a0acf6aa5ba68a13ce8882229ca7cc
Jibbajabbafic <Jibbajabbafic@users.noreply.github.com>
2021-04-25 23:41:03 +0100

    feat: add world_state extrapolation

--- client/world/world.gd
+++ client/world/world.gd
@@ -42,27 +42,58 @@ func _ready():
 func _physics_process(_delta):
 	var render_time = OS.get_system_time_msecs() - INTERPOLATION_OFFSET
 	if world_state_buffer.size() > 1:
-		while world_state_buffer.size() > 2 and render_time > world_state_buffer[1].T:
+		# world_state_buffer[0] will always be the oldest received world_state
+		while world_state_buffer.size() > 2 and render_time > world_state_buffer[2].T:
 			world_state_buffer.remove(0)
-		var interpolation_factor = float(render_time - world_state_buffer[0]["T"]) / float(world_state_buffer[1]["T"] - world_state_buffer[0]["T"])
-		for player in world_state_buffer[1].keys():
-			if str(player) == "T":
-				continue
-			if player == multiplayer.get_network_unique_id():
-				continue
-			if not world_state_buffer[0].has(player):
-				# Connecting player won't be available in past world_state yet
-				continue
-			if has_node(str(player)):
-				# Use the position between past and future world_states
-				var new_position = lerp(
-					world_state_buffer[0][player]["P"],
-					world_state_buffer[1][player]["P"],
-					interpolation_factor
-				)
-				get_node(str(player)).move_player(new_position)
-			else:
-				spawn_player(player, world_state_buffer[1][player]["P"])
+		if world_state_buffer.size() > 2:
+			# We have a future world state available - smooth movement!
+			interpolate_world_state(render_time)
+		elif render_time > world_state_buffer[1].T:
+			# No future world_states available - guess where things will be
+			extrapolate_world_state(render_time)
+
+
+func interpolate_world_state(render_time) -> void:
+	var interpolation_factor = float(render_time - world_state_buffer[1]["T"]) / float(world_state_buffer[2]["T"] - world_state_buffer[1]["T"])
+	for player in world_state_buffer[2].keys():
+		if str(player) == "T":
+			# Ignore the included timestamp
+			continue
+		if player == multiplayer.get_network_unique_id():
+			# Ignore the local player
+			continue
+		if not world_state_buffer[1].has(player):
+			# A connecting player won't be available in past world_state yet
+			continue
+		if has_node(str(player)):
+			# Use the position between past and future world_states to calculate
+			# the current position
+			var new_position = lerp(
+				world_state_buffer[1][player]["P"],
+				world_state_buffer[2][player]["P"],
+				interpolation_factor
+			)
+			get_node(str(player)).move_player(new_position)
+		else:
+			spawn_player(player, world_state_buffer[2][player]["P"])
+
+
+func extrapolate_world_state(render_time) -> void:
+	var extrapolation_factor = float(render_time - world_state_buffer[0]["T"]) / float(world_state_buffer[1]["T"] - world_state_buffer[0]["T"]) - 1.00
+	for player in world_state_buffer[1].keys():
+		if str(player) == "T":
+			# Ignore the included timestamp
+			continue
+		if player == multiplayer.get_network_unique_id():
+			# Ignore the local player
+			continue
+		if not world_state_buffer[0].has(player):
+			# A connecting player won't be available in past world_state yet
+			continue
+		if has_node(str(player)):
+			var position_delta = (world_state_buffer[1][player]["P"] - world_state_buffer[0][player]["P"])
+			var new_position = world_state_buffer[1][player]["P"] + (position_delta * extrapolation_factor)
+			get_node(str(player)).move_player(new_position)
 
 
 func update_world_state(world_state) -> void:
@@ -99,6 +130,7 @@ func spawn_player(player_id, spawn_position, is_master = false):
 
 func despawn_player(player_id):
 	print("[CNT] Despawning player %s" % player_id)
+	yield(get_tree().create_timer(0.2), "timeout")
 	var player = get_node(str(player_id))
 	if player:
 		player.queue_free()
