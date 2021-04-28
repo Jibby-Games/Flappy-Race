3d55e0a3ad5a6f897468b6c799f8d8709f3e2bed
Jibbajabbafic <Jibbajabbafic@users.noreply.github.com>
2021-04-28 22:16:06 +0100

    feat: separate common and client world

--- client/world/world.gd
+++ client/world/world.gd
@@ -1,33 +1,24 @@
-extends Node2D
+extends CommonWorld
 
 
-const WALL := preload("./wall/wall.tscn")
-const PLAYER := preload("./player/player.tscn")
 const INTERPOLATION_OFFSET = 100
+
 
 onready var Background := $Background
 onready var HiScore := $Camera2D/UI/HighScore
 onready var Score := $Camera2D/UI/Score
-onready var WallSpawnTimer := $WallSpawnTimer
 
 
 # World state vars
 var last_world_state := 0
 var world_state_buffer := []
 
-# Wall vars
-var height_range := 100
-var gap_range_min := 130
-var gap_range_max := 250
-var wall_spawn_time := 2.5
-var start_wall_speed := 4.0
-var wall_speed := start_wall_speed
-var speed_up := 0.1
 
 # Background vars
 var scroll_speed := 0.1
 var bob_speed := 1.0
 var bob_amplitude := 0.01
+
 
 func _ready():
 	# Setup gubbins
@@ -102,99 +93,27 @@ func update_world_state(world_state) -> void:
 		world_state_buffer.append(world_state)
 
 
-func start_game(game_seed = null) -> void:
-	print("[CNT] Starting game with seed %s" % game_seed)
-	if game_seed:
-		Globals.set_game_seed(game_seed)
-	else:
-		var _seed = Globals.randomize_game_seed()
-	spawn_player(multiplayer.get_network_unique_id(), Vector2(0, 0), true)
-	for player in multiplayer.get_network_connected_peers():
-		# Don't spawn the server as a player
-		if player != 1:
-			spawn_player(player, Vector2(0, 0))
-	reset_walls()
-
-
-func spawn_player(player_id, spawn_position, is_master = false):
-	if not has_node(str(player_id)):
-		print("[CNT] Spawning player %s" % player_id)
-		var player = PLAYER.instance()
-		player.connect("death", self, "_on_Player_death")
-		player.connect("score_point", self, "_on_Player_score_point")
-		player.name = str(player_id)
-		player.position = spawn_position
-		player.is_master = is_master
-		add_child(player)
-
-
-func despawn_player(player_id):
-	print("[CNT] Despawning player %s" % player_id)
-	yield(get_tree().create_timer(0.2), "timeout")
-	var player = get_node(str(player_id))
-	if player:
-		player.queue_free()
-
-
-func reset_walls():
-	wall_speed = start_wall_speed
-	WallSpawnTimer.wait_time = wall_spawn_time
-	WallSpawnTimer.start()
-
-
 func reset_game() -> void:
 	Network.Client.request_start_game()
 
 
-#### Wall functions
-func spawn_wall() -> void:
-	var inst = WALL.instance()
-	# Use the game RNG to keep the levels deterministic
-	var height = Globals.game_rng.randf_range(-height_range, height_range)
-	var gap = Globals.game_rng.randf_range(gap_range_min, gap_range_max)
-	print("[WORLD]: Spawning wall - height: ", height, " - gap: ", gap)
-	inst.position = Vector2(get_viewport().size.x / 2 + 64, height)
-	inst.gap = gap
-	inst.speed = wall_speed
-	call_deferred("add_child", inst)
-
-
-func _on_WallSpawner_timeout() -> void:
-	WallSpawnTimer.wait_time = wall_spawn_time * float(start_wall_speed) / wall_speed
-	spawn_wall()
-
-
-#### Player helper functions
 func _on_Player_death(player) -> void:
 	# See if we have a new PB
 	if player.score > Globals.high_score:
 		Globals.save_high_score(player.score)
 		HiScore.text = str(player.score)
-
-	# Tell the engine it can lose the player
-	player.queue_free()
 	show_game_over()
+	._on_Player_death(player)
 
 
 func show_game_over() -> void:
-	WallSpawnTimer.stop()
 	$Camera2D/UI/GameOver.show()
 
 
 func _on_Player_score_point(player) -> void:
 	# Actual incrementing is handled on the player object
-	increase_difficulty()
 	Score.text = str(player.score)
-
-
-#### World functions
-func increase_difficulty() -> void:
-	wall_speed += speed_up
-	# Speed up the background
-	# TODO: this causes jerky stutter. Needs fixing.
-	# Background.material.set_shader_param('scroll_speed', wall_speed*parallax)
-	for wall in get_tree().get_nodes_in_group("walls"):
-		wall.speed = wall_speed
+	._on_Player_score_point(player)
 
 
 func _on_BGMusic_finished() -> void:
---
+++ common/world/world.gd
@@ -0,0 +1,97 @@
+extends Node2D
+
+
+class_name CommonWorld
+
+
+export(PackedScene) var Wall
+export(PackedScene) var Player
+
+
+onready var WallSpawnTimer := $WallSpawnTimer
+
+
+# Wall vars
+var height_range := 100
+var gap_range_min := 130
+var gap_range_max := 250
+var wall_spawn_time := 2.5
+var start_wall_speed := 4.0
+var wall_speed := start_wall_speed
+var speed_up := 0.1
+
+
+func start_game(game_seed = null) -> void:
+	print("[COM] Starting game with seed %s" % game_seed)
+	if game_seed:
+		Globals.set_game_seed(game_seed)
+	else:
+		var _seed = Globals.randomize_game_seed()
+	spawn_player(multiplayer.get_network_unique_id(), Vector2(0, 0), true)
+	for player in multiplayer.get_network_connected_peers():
+		# Don't spawn the server as a player
+		if player != 1:
+			spawn_player(player, Vector2(0, 0))
+	reset_walls()
+
+
+func spawn_player(player_id, spawn_position, is_master = false):
+	if not has_node(str(player_id)):
+		print("[COM] Spawning player %s" % player_id)
+		var player = Player.instance()
+		player.connect("death", self, "_on_Player_death")
+		player.connect("score_point", self, "_on_Player_score_point")
+		player.name = str(player_id)
+		player.position = spawn_position
+		player.is_master = is_master
+		add_child(player)
+
+
+func despawn_player(player_id):
+	print("[COM] Despawning player %s" % player_id)
+	yield(get_tree().create_timer(0.2), "timeout")
+	var player = get_node(str(player_id))
+	if player:
+		player.queue_free()
+
+
+#### Wall functions
+func reset_walls():
+	wall_speed = start_wall_speed
+	$WallSpawnTimer.wait_time = wall_spawn_time
+	$WallSpawnTimer.start()
+
+
+func spawn_wall() -> void:
+	var inst = Wall.instance()
+	# Use the game RNG to keep the levels deterministic
+	var height = Globals.game_rng.randf_range(-height_range, height_range)
+	var gap = Globals.game_rng.randf_range(gap_range_min, gap_range_max)
+	print("[WORLD]: Spawning wall - height: ", height, " - gap: ", gap)
+	inst.position = Vector2(get_viewport().size.x / 2 + 64, height)
+	inst.gap = gap
+	inst.speed = wall_speed
+	call_deferred("add_child", inst)
+
+
+func _on_WallSpawner_timeout() -> void:
+	$WallSpawnTimer.wait_time = wall_spawn_time * float(start_wall_speed) / wall_speed
+	spawn_wall()
+
+
+#### Player helper functions
+func _on_Player_death(player) -> void:
+	# Tell the engine it can lose the player
+	player.queue_free()
+	$WallSpawnTimer.stop()
+
+
+func _on_Player_score_point(_player) -> void:
+	increase_difficulty()
+
+
+#### World functions
+func increase_difficulty() -> void:
+	wall_speed += speed_up
+	for wall in get_tree().get_nodes_in_group("walls"):
+		wall.speed = wall_speed
