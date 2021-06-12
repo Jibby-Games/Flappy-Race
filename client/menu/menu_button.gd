c622aa7dd986edf4116bcbb6ed939b3e840b8317
Jibbajabbafic <Jibbajabbafic@users.noreply.github.com>
2021-06-12 11:23:29 +0100

    feat(ui): add alignment to menu button

--- client/menu/menu_button.gd
+++ client/menu/menu_button.gd
@@ -3,10 +3,17 @@ extends Button
 
 
 export(String) var label = "Label" setget set_label
+export(int, "Left", "Center", "Right", "Fill") var alignment setget set_alignment
 
 
 func set_label(new_text: String) -> void:
 	if label != new_text:
 		label = new_text
 		$Label.text = new_text
+		update()
+
+func set_alignment(new_align: int) -> void:
+	if alignment != new_align:
+		alignment = new_align
+		$Label.align = new_align
 		update()
