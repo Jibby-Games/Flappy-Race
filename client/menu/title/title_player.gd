7c82699e2fdb56981ce4908dd745abe0058ee61f
Jibbajabbafic <Jibbajabbafic@users.noreply.github.com>
2021-06-13 21:03:27 +0100

    fix(title): always randomize birds

--- client/menu/title/title_player.gd
+++ client/menu/title/title_player.gd
@@ -2,11 +2,12 @@ extends Node2D
 
 
 func _ready():
+	randomize()
 	reset()
 
 
 func _physics_process(_delta) -> void:
-	# Randomly flap, or when about to go off screen
+	# Flap randomly, or when about to go off screen
 	var do_flap_roll = rand_range(0, 100)
 	if do_flap_roll > 96 or $Player.position.y > get_viewport().size.y:
 		$Player.do_flap()
@@ -19,7 +20,7 @@ func reset() -> void:
 
 func randomise_start_position() -> void:
 	# Reset position to the left side of the screen somewhere
-	var x_pos = rand_range(0, -2000)
+	var x_pos = rand_range(-100, -2000)
 	var y_pos = rand_range(0, get_viewport().size.y)
 	$Player.position = Vector2(x_pos, y_pos)
 
