624751ea8d28250af1a5eecf40a0dddb079771c5
Jibbajabbafic <Jibbajabbafic@users.noreply.github.com>
2021-10-02 15:19:06 +0100

    fix(world): despawn player on finish

--- common/world/world.gd
+++ common/world/world.gd
@@ -152,9 +152,9 @@ func _on_Player_score_point(player: CommonPlayer) -> void:
 
 
 func _on_Player_finish(player: CommonPlayer) -> void:
-	Logger.print(self, "Player %s crossed the finish line!" % player.name)
-	player.disconnect("death", self, "_on_Player_death")
-	spawned_players.erase(player)
+	var player_id = int(player.name)
+	Logger.print(self, "Player %s crossed the finish line!" % player_id)
+	despawn_player(player_id)
 	if spawned_players.size() == 0:
 		end_race()
 
