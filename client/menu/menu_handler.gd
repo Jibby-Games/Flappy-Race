e755a5adea66af875d399f490782f0cbc7f728d7
Jibbajabbafic <Jibbajabbafic@users.noreply.github.com>
2021-12-11 16:01:44 +0000

    feat(menu): add scrolling cloud background
    Also tweaked the background sky colour so it didn't match one of the
    player colours.

--- client/menu/menu_handler.gd
+++ client/menu/menu_handler.gd
@@ -6,11 +6,18 @@ export (PackedScene) var title_scene
 
 var current_menu: PackedScene
 var previous_menu: PackedScene
-
+var scroll_x = 0
+var scroll_speed = 200
 
 func _ready() -> void:
 	_change_menu_to(title_scene)
 	$MusicPlayer.play_track_name("Drozerix - Digital Rendezvous")
+
+
+func _process(delta: float) -> void:
+	# This makes the parallax background move without a character
+	scroll_x -= delta * scroll_speed
+	$ParallaxBackground.scroll_offset.x = scroll_x
 
 
 func _change_menu_to(next_scene: PackedScene) -> void:
