0e7412bdace6692d85192d85791943e39cb36eca
Jibbajabbafic <Jibbajabbafic@users.noreply.github.com>
2022-01-22 22:40:06 +0000

    fix(globals): add ready function to fix release export

--- common/singletons/globals.gd
+++ common/singletons/globals.gd
@@ -4,6 +4,10 @@ extends Node
 const CLIENT_SCRIPT = "res://client/singletons/globals.gd"
 
 
+func _ready() -> void:
+	pass
+
+
 func change_to_client() -> void:
 	var script = load(CLIENT_SCRIPT)
 	self.set_script(script)
