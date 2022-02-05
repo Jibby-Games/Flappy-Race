7eb32513dd87a088f0347bcfa001365077fd8db9
Jibbajabbafic <Jibbajabbafic@users.noreply.github.com>
2022-02-05 16:04:38 +0000

    fix(wall): only allow each wall to score once

--- client/world/wall/wall.gd
+++ client/world/wall/wall.gd
@@ -5,3 +5,9 @@ func set_gap(size: float) -> void:
 	.set_gap(size)
 	$UpperSprite.position.y = $UpperCollider.position.y
 	$LowerSprite.position.y = $LowerCollider.position.y
+
+
+func _on_PointArea_body_entered(body:Node) -> void:
+	# Only disable score zone for the local client
+	if int(body.name) == multiplayer.get_network_unique_id():
+		$PointArea/CollisionShape2D.set_deferred("disabled", true)
