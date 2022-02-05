817e5a7640bab4595b130014de91e75d03d623f8
Jibbajabbafic <Jibbajabbafic@users.noreply.github.com>
2022-02-05 14:39:22 +0000

    fix(world): reduce wall spawn range

--- common/world/world.gd
+++ common/world/world.gd
@@ -19,7 +19,7 @@ var height_range := 100
 var gap_range_min := 130
 var gap_range_max := 250
 var wall_spacing := 400
-var wall_spawn_range := 5000
+var wall_spawn_range := 3000
 var starting_wall_pos := 1500
 var next_wall_pos := starting_wall_pos
 
