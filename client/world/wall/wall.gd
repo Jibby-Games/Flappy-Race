a2e48940e01c7019d62712a913c35f205f38da43
Jibbajabbafic <Jibbajabbafic@users.noreply.github.com>
2022-03-12 11:15:11 +0000

    fix(walls): stop multiple points from each server wall

--- client/world/wall/wall.gd
+++ client/world/wall/wall.gd
@@ -5,10 +5,3 @@ func set_gap(size: float) -> void:
 	.set_gap(size)
 	$UpperSprite.position.y = $UpperCollider.position.y
 	$LowerSprite.position.y = $LowerCollider.position.y
-
-
-func _on_PointArea_body_entered(body:Node) -> void:
-	._on_PointArea_body_entered(body)
-	# Only disable score zone for the local client
-	if int(body.name) == multiplayer.get_network_unique_id():
-		$PointArea/CollisionShape2D.set_deferred("disabled", true)
