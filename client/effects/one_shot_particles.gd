c0e560a13e1ffbf72cb29ca6ef31757ab3af5823
Jibbajabbafic <Jibbajabbafic@users.noreply.github.com>
2022-08-22 23:13:12 +0100

    feat(world): add confetti on race start and finish

--- client/effects/one_shot_particles.gd
+++ client/effects/one_shot_particles.gd
@@ -1,6 +1,11 @@
 extends Particles2D
 
+# How long to wait before destroying the particles to avoid cutting them off
+var delay := 1.0
+var time := 0.0
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	if not emitting:
-		queue_free()
+		time += delta
+		if time > delay:
+			queue_free()
