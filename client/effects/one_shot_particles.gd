aab4e676ac8461fee7dad9ff516ebbfaf10f6ceb
Jibbajabbafic <Jibbajabbafic@users.noreply.github.com>
2022-08-26 17:08:50 +0100

    fix(particles): stop offscreen visibility issue
    Particles wouldn't spawn if off screen and then would start playing when
    on screen again which doesn't look right, fixed it by forcing an
    infinite visibility rect.
    Also improves the confetti particle animation to loop properly.

--- client/effects/one_shot_particles.gd
+++ client/effects/one_shot_particles.gd
@@ -4,6 +4,13 @@ extends Particles2D
 var delay := 1.0
 var time := 0.0
 
+
+func _init() -> void:
+	# Force particles to always run even when off screen otherwise they spawn
+	# when back on screen which looks odd
+	self.set_visibility_rect(Rect2(-INF, -INF, INF, INF))
+
+
 func _process(delta: float) -> void:
 	if not emitting:
 		time += delta
