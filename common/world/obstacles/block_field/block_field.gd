faf647ebb81b31d1435ce4c69a9bd1dc24fde659
Jibbajabbafic <Jibbajabbafic@users.noreply.github.com>
2022-08-29 15:25:57 +0100

    feat(obstacle): add coins to block fields

--- common/world/obstacles/block_field/block_field.gd
+++ common/world/obstacles/block_field/block_field.gd
@@ -5,11 +5,13 @@ tool
 const BLOCK_SIZE := 64
 
 export(PackedScene) var Block
+export(PackedScene) var CoinSpawner
 export(float) var block_density := 0.075 setget set_block_density
 export(int) var field_length := 800 setget set_field_length
 export(int) var field_height := 1280 setget set_field_height
 export(bool) var generate_blocks := false setget _do_generate_blocks
 export(int) var checkpoint_distance := 128 setget set_checkpoint_distance
+export(int) var max_coins := 3 setget set_max_coins
 
 
 var blocks := []
@@ -27,8 +29,9 @@ func do_generate(game_rng: RandomNumberGenerator) -> void:
 	blocks.clear()
 	points.clear()
 	var blocks_to_spawn: int = int(field_length * field_height * block_density / (BLOCK_SIZE*BLOCK_SIZE))
+	var coins_to_spawn: int = game_rng.randi_range(0, max_coins)
+	var coins := 0
 	for i in blocks_to_spawn:
-		var block: Node2D = Block.instance()
 		var overlap := true
 		var iters := 0
 		var pos: Vector2
@@ -49,9 +52,15 @@ func do_generate(game_rng: RandomNumberGenerator) -> void:
 			continue
 		if failed_blocks > 0:
 			print("Failed to place %d blocks" % failed_blocks)
-		block.position = pos
-		blocks.append(block)
-		add_child(block)
+		var inst: Node2D
+		if coins < coins_to_spawn:
+			coins += 1
+			inst = CoinSpawner.instance()
+		else:
+			inst = Block.instance()
+		inst.position = pos
+		blocks.append(inst)
+		add_child(inst)
 		points.append(pos)
 	$"%Checkpoint".position.x = field_length + checkpoint_distance
 	$"%PointArea".position.x = $"%Checkpoint".position.x
@@ -85,7 +94,14 @@ func set_checkpoint_distance(value: int) -> void:
 		_do_generate_blocks()
 
 
+func set_max_coins(value: int) -> void:
+	max_coins = value
+	if Engine.is_editor_hint():
+		_do_generate_blocks()
+
+
 func _do_generate_blocks(_value: bool = false) -> void:
 	# Create RNG for testing purposes
 	var rng := RandomNumberGenerator.new()
+	rng.randomize()
 	generate(rng)
