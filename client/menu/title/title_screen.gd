53bd53431a621b96086aea2dc92e6612d94de001
Jibbajabbafic <Jibbajabbafic@users.noreply.github.com>
2022-09-14 09:08:35 +0100

    feat(title): improve background players

--- client/menu/title/title_screen.gd
+++ client/menu/title/title_screen.gd
@@ -4,23 +4,50 @@ extends MenuControl
 var singleplayer_scene := "res://client/menu/setup/singleplayer/singleplayer_setup.tscn"
 var multiplayer_scene := "res://client/menu/lobby/lobby.tscn"
 var options_scene := "res://client/menu/options/options.tscn"
+var title_player := preload("res://client/menu/title/title_player.tscn")
+# Aim for about 32 players
+var area_per_player := (1920 * 1080) / 32
+var spawned_title_players := []
 
 
 func _ready() -> void:
 	$Menu/VersionLabel.text = ProjectSettings.get_setting("application/config/version")
 	$Menu/Buttons/SingleplayerButton.grab_focus()
+	var result := get_viewport().connect("size_changed", self, "spawn_title_players")
+	assert(result == OK)
+	spawn_title_players()
 
 
-func _on_SingleplayerButton_pressed():
+func spawn_title_players() -> void:
+	var total_players := get_viewport_rect().get_area() / area_per_player
+	Logger.print(self, "Viewport size changed - Need %d players" % total_players)
+	if total_players > spawned_title_players.size():
+		# Add more players
+		var players_to_spawn := total_players - spawned_title_players.size()
+		Logger.print(self, "Spawning %d players" % players_to_spawn)
+		for i in players_to_spawn:
+			var inst = title_player.instance()
+			spawned_title_players.append(inst)
+			$PlayerContainer.add_child(inst)
+	elif total_players < spawned_title_players.size():
+		# Remove some players
+		var players_to_despawn := spawned_title_players.size() - total_players
+		Logger.print(self, "Removing %d players" % players_to_despawn)
+		for i in players_to_despawn:
+			var player = spawned_title_players.pop_back()
+			player.remove_when_off_screen = true
+
+
+func _on_SingleplayerButton_pressed() -> void:
 	change_menu(singleplayer_scene)
 	Network.start_singleplayer()
 
 
-func _on_MultiplayerButton_pressed():
+func _on_MultiplayerButton_pressed() -> void:
 	change_menu(multiplayer_scene)
 
 
-func _on_OptionsButton_pressed():
+func _on_OptionsButton_pressed() -> void:
 	change_menu(options_scene)
 
 
