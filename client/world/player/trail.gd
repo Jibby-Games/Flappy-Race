6c5c9844e9c8b635113d8ef200a8edc22c90ea1d
Jibbajabbafic <Jibbajabbafic@users.noreply.github.com>
2022-09-20 22:24:57 +0100

    fix(player): make trail use physics process to make it consistent

--- client/world/player/trail.gd
+++ client/world/player/trail.gd
@@ -1,7 +1,7 @@
 extends Line2D
 
 
-var max_points := 5
+var max_points := 6
 var clear_distance := 10000
 var last_position := Vector2.ZERO
 
@@ -10,7 +10,7 @@ func _ready() -> void:
 	set_as_toplevel(true)
 
 
-func _process(_delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	var current_position : Vector2 = get_parent().position
 	# Stops the trail streaking across the screen if it teleports
 	if last_position.distance_squared_to(current_position) > clear_distance:
