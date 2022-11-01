c0a1a9c631562721dea35b6a317b34e984c6c175
Jibbajabbafic <Jibbajabbafic@users.noreply.github.com>
2022-11-01 23:28:53 +0000

    refactor(main): move arg parsing to main node

--- common/main.gd
+++ common/main.gd
@@ -2,7 +2,48 @@ extends Node
 
 # This node is only needs to detect the command line args so the right scene can be loaded
 func _ready() -> void:
-	if "--server" in OS.get_cmdline_args():
-		Network.change_to_server()
+	parse_command_line_args()
+
+
+func parse_command_line_args() -> void:
+	# Server options
+	var is_server := OS.has_feature("Server")
+
+	# Client options
+	var host_game := false
+	var join_ip := ""
+
+	# Hosting options
+	var port := Network.RPC_PORT
+	var use_upnp := false
+
+	var args := OS.get_cmdline_args()
+	for arg_index in args.size():
+		var arg = args[arg_index]
+		if arg == "--server":
+			is_server = true
+		elif arg == "--host":
+			host_game = true
+		elif arg == "--join":
+			join_ip = args[arg_index + 1]
+			if ":" in join_ip:
+				var parts := join_ip.split(":")
+				join_ip = parts[0]
+				port = int(parts[1])
+		elif arg == "--port":
+			port = int(args[arg_index + 1])
+		elif arg == "--upnp":
+			use_upnp = true
+
+	if is_server:
+		Logger.print(self, "Starting Server...")
+		Network.start_server(port, use_upnp)
 	else:
+		Logger.print(self, "Starting Client...")
 		Network.change_to_client()
+		if host_game:
+			Network.Client.change_scene_to_lobby()
+			Network.start_multiplayer_host(port, use_upnp)
+		elif join_ip.empty() == false:
+			Network.Client.change_scene_to_lobby()
+			Network.Client.start_client(join_ip, port)
--- common/singletons/network.gd
+++ common/singletons/network.gd
@@ -11,55 +11,6 @@ var Client: ClientNetwork
 var Server: ServerNetwork
 
 
-func _ready() -> void:
-	parse_command_line_args()
-
-
-func parse_command_line_args() -> void:
-	# Make sure at least one of the two is set before continuing
-	if Client == null or Server == null:
-		yield(get_tree(), "idle_frame")
-
-	# Server options
-	var is_server := false
-
-	# Client options
-	var host_game := false
-	var join_ip := ""
-
-	# Hosting options
-	var port := RPC_PORT
-	var use_upnp := false
-
-	var args := OS.get_cmdline_args()
-	for arg_index in args.size():
-		var arg = args[arg_index]
-		if arg == "--server":
-			is_server = true
-		elif arg == "--host":
-			host_game = true
-		elif arg == "--join":
-			join_ip = args[arg_index + 1]
-			if ":" in join_ip:
-				var parts := join_ip.split(":")
-				join_ip = parts[0]
-				port = int(parts[1])
-		elif arg == "--port":
-			port = int(args[arg_index + 1])
-		elif arg == "--upnp":
-			use_upnp = true
-
-	if is_server:
-		start_server(port, use_upnp)
-	else:
-		if host_game:
-			Client.change_scene_to_lobby()
-			start_multiplayer_host(port, use_upnp)
-		elif join_ip.empty() == false:
-			Client.change_scene_to_lobby()
-			Client.start_client(join_ip, port)
-
-
 func _load_network_scene(scene_path: String) -> SceneHandler:
 	var scene: Node = load(scene_path).instance()
 	get_tree().get_root().call_deferred("add_child", scene)
@@ -70,12 +21,6 @@ func change_to_client() -> void:
 	Globals.change_to_client()
 	var result: int
 	result = get_tree().change_scene(CLIENT_NETWORK)
-	assert(result == OK)
-
-
-func change_to_server() -> void:
-	var result: int
-	result = get_tree().change_scene(SERVER_NETWORK)
 	assert(result == OK)
 
 
