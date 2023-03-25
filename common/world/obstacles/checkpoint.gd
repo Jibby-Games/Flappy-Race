2bfc9a9437ccab677e296e779e37615001b3ad28
Jibbajabbafic <Jibbajabbafic@users.noreply.github.com>
2023-03-25 11:18:43 +0000

    fix(world): don't set the same checkpoint multiple times

--- common/world/obstacles/checkpoint.gd
+++ common/world/obstacles/checkpoint.gd
@@ -2,5 +2,8 @@ extends Area2D
 
 
 func _on_Checkpoint_body_entered(body: Node) -> void:
+	if body.checkpoint_position == global_position:
+		# Already set this checkpoint
+		return
 	body.checkpoint_position = global_position
 	Logger.print(self, "Set player %s checkpoint to %s", [body.name, body.checkpoint_position])
