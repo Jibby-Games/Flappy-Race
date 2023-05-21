f26585a0c6e768cc7b0498dcb76e12786047a987
Jibbajabbafic <Jibbajabbafic@users.noreply.github.com>
2023-05-21 18:06:19 +0100

    fix(bot): stop bots trying to go out of bounds

--- common/world/bot/bot_controller.gd
+++ common/world/bot/bot_controller.gd
@@ -5,7 +5,8 @@ class_name BotController
 var bottom_edge: int = ProjectSettings.get_setting("display/window/size/height") / 2
 var player: CommonPlayer
 var target_pos := Vector2(100000, 0)
-var target_marker
+var target_marker: Line2D
+var last_nav_pos := Vector2.ZERO
 var below_target_threshold := 16
 var ahead_of_target_threshold := 128
 var flap_detector_bodies := []
@@ -27,9 +28,6 @@ func _ready():
 
 
 func _physics_process(delta: float) -> void:
-	if not player.enable_movement:
-		# Player can't move yet so take no action
-		return
 	$FlapDetector.position = Vector2(player.velocity.x, -player.FLAP).normalized() * flap_lookahead
 	$ForwardDetector.position = player.velocity.normalized() * forward_lookahead
 	# Add a cooldown so flaps don't get spammed
@@ -38,7 +36,7 @@ func _physics_process(delta: float) -> void:
 		if flap_timer >= flap_cooldown_seconds:
 			in_flap_cooldown = false
 			flap_timer = 0.0
-	elif should_flap():
+	elif player.enable_movement and should_flap():
 		in_flap_cooldown = true
 		player.do_flap()
 		if Network.Server:
@@ -68,14 +66,24 @@ func should_flap() -> bool:
 	if not forward_detector_bodies.empty():
 		if verbose_bot: print("flap due to forward detector")
 		return true
+
+	# Calculate the next nav pos
 	var nav_pos := nav_agent.get_next_location()
+	# Just carry on straight if nav pos is out of bounds
+	if nav_pos.y > bottom_edge or nav_pos.y < -bottom_edge:
+		nav_pos.y = last_nav_pos.y
 	draw_debug_point(nav_pos, Color.red)
+	last_nav_pos = nav_pos
+	if verbose_bot: print_debug("%s nav point: %s" % [player.name, nav_pos])
+
 	if player.global_position.x + ahead_of_target_threshold >= nav_pos.x:
+		# Recalculate next target location if near the next nav pos
 		nav_agent.set_target_location(target_pos)
 	if nav_pos.y + below_target_threshold < player.global_position.y:
 		if verbose_bot: print("flap due to below nav target")
 		return true
 	return false
+
 
 func draw_debug_point(pos: Vector2, col: Color = Color.white) -> void:
 	if target_marker:
@@ -93,7 +101,7 @@ func is_near_bottom() -> bool:
 	return player.position.y >= bottom_edge
 
 
-func _on_player_death(_player) -> void:
+func _on_player_death(_player: CommonPlayer) -> void:
 	#TODO randomize route just in case we get stuck on one
 	nav_agent.set_target_location(target_pos)
 
