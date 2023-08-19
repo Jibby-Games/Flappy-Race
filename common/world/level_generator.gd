dc2958a6fc8b46599e560e6e9202664fda70cf37
Jibbajabbafic <Jibbajabbafic@users.noreply.github.com>
2023-08-19 11:39:40 +0100

    feat(world): add coin line obstacles

--- common/world/level_generator.gd
+++ common/world/level_generator.gd
@@ -4,8 +4,22 @@ tool
 
 class_name LevelGenerator
 
-export(Array) var Obstacles
-export(Array) var ObstacleRandomWeights
+export(Array) var Obstacles = [
+	preload("res://common/world/obstacles/wall/wall.tscn"),
+	preload("res://common/world/obstacles/block_field/block_field.tscn"),
+	preload("res://common/world/obstacles/coin_circle/coin_circle.tscn"),
+	preload("res://common/world/obstacles/tunnel/tunnel.tscn"),
+	preload("res://common/world/obstacles/item_circle/item_circle.tscn"),
+	preload("res://common/world/obstacles/spawner_line/spawner_line.tscn"),
+]
+export(Array) var ObstacleRandomWeights = [
+	100,
+	50,
+	25,
+	50,
+	25,
+	100,
+]
 export(PackedScene) var StartLine
 export(PackedScene) var FinishLine
 
---
+++ common/world/obstacles/spawner_line/spawner_line.gd
@@ -0,0 +1,67 @@
+extends Obstacle
+
+tool
+
+const MAX_LINE_HEIGHT := 1000.0
+const MIN_SPACING_PER_ITEM := (1000 / 8)
+const MIN_ITEMS := 1
+const MAX_ITEMS := int(MAX_LINE_HEIGHT / MIN_SPACING_PER_ITEM)
+
+export(PackedScene) var item_scene := preload("res://client/world/coin_spawner/coin_spawner.tscn")
+export(float) var line_height := MAX_LINE_HEIGHT setget set_line_height
+export(int) var items := 12 setget set_items
+
+var spawned_items := []
+
+
+func _ready() -> void:
+	if Engine.is_editor_hint():
+		generate_line(line_height, items)
+
+
+func generate_line(new_height: float, num_items: int) -> void:
+	for item in spawned_items:
+		item.queue_free()
+	spawned_items.clear()
+	# -1 from num_items since we need to calculate the number of gaps between
+	var increment := new_height / (num_items - 1) if (num_items > 1) else 0.0
+	for i in num_items:
+		var inst = item_scene.instance()
+		# Evenly space items
+		inst.position = Vector2(0, (i*increment) - (new_height/2.0))
+		add_child(inst)
+		spawned_items.append(inst)
+
+
+func do_generate(_game_rng: RandomNumberGenerator) -> void:
+	# Randomise num items and then calculate line height
+	assert(MIN_ITEMS <= MAX_ITEMS, "Maximum number of items must be greater than %d! Got: %d" % [MIN_ITEMS, MAX_ITEMS])
+	items = _game_rng.randi_range(MIN_ITEMS, MAX_ITEMS)
+	line_height = items * MIN_SPACING_PER_ITEM
+	if items < MAX_ITEMS:
+		# Randomise the height if there's space
+		# Assuming max items fills the whole screen
+		var edge_gap := (boundary_height - line_height) / 2
+		self.position.y = _game_rng.randf_range(-edge_gap, edge_gap)
+	generate_line(line_height, items)
+
+
+func generate_navigation_polygon() -> NavigationPolygon:
+	var nav_poly := NavigationPolygon.new()
+	nav_poly.add_outline(get_boundary_poly())
+	nav_poly.make_polygons_from_outlines()
+	return nav_poly
+
+
+func calculate_length() -> int:
+	return $"%Checkpoint".position.x
+
+
+func set_line_height(value: float) -> void:
+	line_height = value
+	generate_line(line_height, items)
+
+
+func set_items(value: int) -> void:
+	items = value
+	generate_line(line_height, items)
