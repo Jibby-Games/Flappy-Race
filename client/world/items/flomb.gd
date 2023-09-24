93cc28c2d1ab9d0d2755a302f3390f513dd161dd
Jibbajabbafic <Jibbajabbafic@users.noreply.github.com>
2023-09-24 14:30:20 +0100

    feat(items): flomb animation

--- client/world/items/flomb.gd
+++ client/world/items/flomb.gd
@@ -2,8 +2,12 @@ extends "res://common/world/items/flomb.gd"
 
 export(PackedScene) var explosion_effect = preload("res://client/effects/explosion/explosion.tscn")
 
+func _ready() -> void:
+	$AnimatedSprite.play()
+
+
 func explode() -> void:
-	$Sprite.hide()
+	$AnimatedSprite.hide()
 	# Spawn an explosion
 	var inst: Node2D = explosion_effect.instance()
 	inst.global_position = self.global_position
