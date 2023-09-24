71e7e399707899ed0675412d040d7070353ce8f8
Jibbajabbafic <Jibbajabbafic@users.noreply.github.com>
2023-09-24 18:06:39 +0100

    fix(spawners): stops items being taken multiple times

--- common/world/spawners/pickups/pickup.gd
+++ common/world/spawners/pickups/pickup.gd
@@ -1,7 +1,13 @@
 extends Area2D
 
+var is_taken := false
+
 
 func _on_Pickup_body_entered(body: Node) -> void:
+	# Stop one pickup being taken multiple times
+	if is_taken:
+		return
+	is_taken = true
 	_on_item_taken(body)
 
 
