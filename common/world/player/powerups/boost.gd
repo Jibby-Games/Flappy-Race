5312937650533e6bf866b00563908be965a9282c
Jibbajabbafic <Jibbajabbafic@users.noreply.github.com>
2023-09-26 23:59:55 +0100

    fix(items): make boost work on remote clients

--- common/world/player/powerups/boost.gd
+++ common/world/player/powerups/boost.gd
@@ -2,7 +2,6 @@ extends Powerup
 
 const WALL_COLLISION_LAYER := 1
 const BOOST_AMOUNT := 2000
-const ACCELERATION_TIME := 0.5
 
 
 func _ready() -> void:
@@ -11,16 +10,11 @@ func _ready() -> void:
 
 func _activate() -> void:
 	self.show()
-	# Gradually increase the player's speed
-	var tween = create_tween()
-	tween.tween_property(player, "acceleration:x", BOOST_AMOUNT, ACCELERATION_TIME)
+	player.acceleration.x = BOOST_AMOUNT
 	player.set_enable_wall_collisions(false)
 
 
 func _deactivate() -> void:
 	self.hide()
-	# Gradually slow down the player
-	var tween = create_tween()
-	tween.tween_property(player, "acceleration:x", 0, ACCELERATION_TIME)
-	# Enable collisions again after decelleration
-	tween.tween_callback(player, "set_enable_wall_collisions", [true])
+	player.acceleration.x = 0
+	player.set_enable_wall_collisions(true)
